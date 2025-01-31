c5063704a7fb049907efeca88115bda7c0db7005
nrlsalsabl <nurulsalsabila315@gmail.com>
2025-01-31 11:38:24 +0700

    payment page

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,2 +1,21 @@
 import 'package:flutter/material.dart';
 
+class PaymentPage extends StatefulWidget {
+  const PaymentPage({super.key});
+
+  @override
+  State<PaymentPage> createState() => _PaymentState();
+}
+
+class _PaymentState extends State<PaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+      title: const Text("Checkout"),
+    ),
+    );
+  }
+}
+
